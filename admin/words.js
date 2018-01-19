832f5a008b2c9fed7cedbb988eb80551b5ce3d16
foxthefox <github@wysiwis.net>
2018-01-19 23:21:09 +0100

    adminv3

---
+++ admin/words.js
@@ -0,0 +1,11 @@
+<!-- you have to define 2 functions in the global scope: -->
+    // Dictionary (systemDictionary is global variable from adapter-settings.js)
+    systemDictionary = {
+        "lifx adapter settings": {"de": "lifx Adapter Einstellung", "en": "lifx adapter setup","ru": "Настройки lifx Драйвера"},
+	                "Lifx lamp(s)": {"de": "Lifx Lampe(n)", "en": "Lifx lamp(s)"},
+	"will be found automatically": {"de": "werden automatisch gefunden", "en": "will be found automatically"},
+        "on save adapter restarts with new config immediately": {
+            "de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
+		"en": "on save adapter restarts with new config immediately",
+            "ru": "Сразу после сохранения настроек драйвер перезапуститься с новыми значениями"
+        }
